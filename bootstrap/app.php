2137503943922987235d52adfec9cf08cf5e3793
M. Pandu Gautama Wiranagara <57618528+pandu9aga@users.noreply.github.com>
2025-11-18 09:27:08 +0700

    disable put env

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,16 +3,21 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Env;
+
+if (php_sapi_name() === 'apache2handler' && ($_SERVER['WINDIR'] ?? false)) {
+    Env::disablePutenv();
+}
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
-        commands: __DIR__ . '/../routes/console.php',
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
+    ->withMiddleware(function (Middleware $middleware): void {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions) {
+    ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
