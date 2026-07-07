a5fae0c9b6b7e00285c6b0483a8cc3a0d409ec2d
M. Pandu Gautama Wiranagara <57618528+pandu9aga@users.noreply.github.com>
2026-07-07 09:07:47 +0700

    animation only in login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,30 @@
 @extends('layouts.app')
 
+@push('head')
+<script src="{{ asset('js/anime.min.js') }}"></script>
+<style>
+    #particle-container {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        overflow: hidden;
+        pointer-events: none;
+        z-index: 1;
+    }
+    .bg-particle {
+        position: absolute;
+        clip-path: polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%);
+        filter: blur(4px);
+        pointer-events: none;
+        will-change: transform;
+    }
+</style>
+@endpush
+
 @section('content')
+<div id="particle-container"></div>
 <div class="auth-container">
     <div class="auth-box">
         <a href="{{ route('index') }}" style="display:block;text-align:center;">
@@ -52,3 +76,63 @@
     </div>
 </div>
 @endsection
+
+@push('scripts')
+<script>
+    (function() {
+        var particleCount = 20;
+        var colors = [
+            '236, 5, 125',
+            '200, 50, 150',
+            '161, 94, 207',
+            '92, 124, 250',
+            '0, 180, 200',
+            '180, 100, 200',
+            '255, 100, 150',
+        ];
+        function rand(min, max) { return Math.random() * (max - min) + min; }
+        function randInt(min, max) { return Math.floor(rand(min, max + 1)); }
+        function createParticle() {
+            var el = document.createElement('div');
+            el.className = 'bg-particle';
+            var size = rand(30, 150);
+            var color = colors[randInt(0, colors.length - 1)];
+            var opacity = rand(0.03, 0.09);
+            el.style.width = size + 'px';
+            el.style.height = size + 'px';
+            
+            var winW = window.innerWidth;
+            var winH = window.innerHeight;
+            var leftPos = rand(-10, 110) / 100 * winW;
+            var topPos = rand(0, 120) / 100 * winH;
+            
+            el.style.left = leftPos + 'px';
+            el.style.top = topPos + 'px';
+            el.style.background = 'rgba(' + color + ', ' + opacity + ')';
+            
+            var container = document.getElementById('particle-container');
+            if (container) {
+                container.appendChild(el);
+            } else {
+                document.body.appendChild(el);
+            }
+            
+            var distance = topPos + winH + 200;
+            anime({
+                targets: el,
+                translateY: -distance,
+                rotate: rand(-30, 30),
+                duration: randInt(12000, 28000),
+                easing: 'linear',
+                complete: function() {
+                    el.remove();
+                    createParticle();
+                }
+            });
+        }
+        for (var i = 0; i < particleCount; i++) {
+            setTimeout(createParticle, randInt(0, 6000));
+        }
+    })();
+</script>
+@endpush
